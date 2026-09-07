91885b9550df854de30f5f8e292254776a2f657b
pmgokul7 <pmgokul7@gmail.com>
2026-09-07 16:49:47 +0530

    socket fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5055',
+        target: 'https://huntlo-communication-server-smoky.vercel.app',
         changeOrigin: true,
         ws: true,
       },
